6fe060209d1ab9e2ecd61a257d4748511e38f7fd
Basem <basemsisy@gmail.com>
2023-07-04 22:33:11 +0300

    feat(cart): scaffolding cart module

---
+++ cart/webpack.config.js
@@ -0,0 +1,21 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const ModuleFedrationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+module.exports = {
+  mode: "development",
+  devServer: {
+    port: 8082,
+  },
+  plugins: [
+    new ModuleFedrationPlugin({
+        name: "cart",
+        filename: "remoteEntry.js",
+        exposes: {
+          "./CartIndex": "./src/index",
+        },
+    }),
+    new HTMLWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
