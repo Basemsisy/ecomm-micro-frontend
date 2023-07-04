c2d9ac2cc889fa28ab7d31ea272c36ebdb20ff8c
Basem <basemsisy@gmail.com>
2023-07-04 23:32:28 +0300

    refactor: use datatype rather than random due to depreciation

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,4 +1,4 @@
 import faker from "faker";
 
-const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
+const cartText = `<div>You have ${faker.datatype.number()} items in your cart</div>`;
 document.getElementById("dev-cart").innerHTML = cartText;
