752a52a868d46a058c2eb7de3aa7f41ceb60baf1
Basem <basemsisy@gmail.com>
2023-07-09 18:36:26 +0300

    refactor(card): add the ability to container app to decide when & where to render card module

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,4 +1,16 @@
 import faker from "faker";
 
-const cartText = `<div>You have ${faker.datatype.number()} items in your cart</div>`;
-document.getElementById("dev-cart").innerHTML = cartText;
+const mount = (el) => {
+  const cartText = `<div>You have ${faker.datatype.number()} items in your cart</div>`;
+  el.innerHTML = cartText;
+};
+
+if (process.env.NODE_ENV === "development") {
+    let el = document.getElementById("dev-cart");
+    if (el) {
+      mount(el);
+    }
+  }
+
+
+export { mount };
